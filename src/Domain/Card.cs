6cf9205d7941e04657bc4c396695a1179380d12c
Flor <florwalravens@hotmail.com>
2021-10-19 09:18:19 +0200

    Finished app

---
+++ src/Domain/Card.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain
+{
+    public class Card
+    {
+
+        #region Properties
+        public FaceValue FaceValue { get; }
+        public Suit Suit { get; }
+        #endregion
+
+        #region Constructors
+        public Card(Suit suit, FaceValue faceValue)
+        {
+            Suit = suit;
+            FaceValue = faceValue;
+        }
+        #endregion
+
+    }
+}
---
+++ src/Domain/GameState.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public enum GameState
+    {
+        GameOver = 1,
+        PlayerPlays = 2,
+        DealerPlays = 3
+    }
+}
